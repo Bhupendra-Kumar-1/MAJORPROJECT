be1e65a58c5ddad681c4c5f32bb61bcd89c8b1c3
Bhupendra Kumar <bhupendra1kumar1@gmail.com>
2026-05-14 23:43:02 +0530

    fix edit delete buttons

--- init/index.js
+++ init/index.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLASDB_URL;
 
 main()
     .then(() =>{
@@ -18,7 +20,7 @@ main()
 
     const initDB = async () => {
         await Listing.deleteMany({});
-        initData.data = initData.data.map((obj) => ({ ...obj, owner: '6a01aea49fc5dce5d11433d6' }));
+        initData.data = initData.data.map((obj) => ({ ...obj, owner: '6a0607c648b2111e5ba06c7d' }));
         await Listing.insertMany(initData.data);
         console.log("data was initialized");
     };
